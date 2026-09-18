d1e17bcc44d11b7700add7bbdfc85a6361ccbafb
Ethan John <c.ethanjohn@gmail.com>
2026-09-18 20:26:50 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// woah, what are you doing here?
+// if you see this, text john the number 6.02 * 10^23!
+
+function removeBlur() {
+    blurred = document.querySelectorAll('[class*="blur"]')[0];
+    blurred.className = "";
+
+    if (!blurred)
+        setTimeout(removeBlur, 1024);
+}
+
+setTimeout(removeBlur, 512); // i like powers of two they make me feel cool
