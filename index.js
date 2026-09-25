3f7969ca8ddcd6df39aed96064fb30f8d0caa6b4
Ethan John <c.ethanjohn@gmail.com>
2026-09-25 11:17:01 +0400

    FUUUUUUUCK

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ function removeBlur() {
     blurred = document.querySelectorAll('[class*="blur"]')[0];
     blurred.className = "";
 
-    if (!blurred)
-        setTimeout(removeBlur, 1024);
+    setTimeout(removeBlur, 1024);
 }
 
-setTimeout(removeBlur, 512); // i like powers of two they make me feel cool
+setTimeout(removeBlur, 1024); // i like powers of two they make me feel cool
